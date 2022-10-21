e23e68646a8c397949e657ec7cc843df7d4bf64e
Mauro <maurometzdasilva@ienh.com.br>
2022-10-21 10:49:55 -0300

    1049

--- src/shared/infra/typeorm/migrations/1665489480969-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1665489480969-CreateUsers.ts
@@ -15,7 +15,8 @@ export class CreateUsers1665489480969 implements MigrationInterface {
                 columns: [
                     {
                         name: "id",
-                        type: "uuid"
+                        type: "uuid",
+                        isPrimary: true,
                     },
                     {
                         name: "name",
